82dfc1f2616bd0c2407233b62a7d3083a801bcba
Wekiban <wes.banyard@googlemail.com>
2021-06-15 08:45:24 +0200

    api call functioning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import RecipeCard from './Components/RecipeCard';
+import { recipeSearch } from './data';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+
+const API_KEY = process.env.REACT_APP_EDAMAM_API_KEY;
+const APP_ID = process.env.REACT_APP_EDAMAM_APP_ID;
+const url = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=`;
 
 function App() {
+  const [data, setData] = useState(recipeSearch);
+  const [query, setQuery] = useState('pizza');
+
+  /* useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(url + query);
+      response = await response.json(); 
+      setData(recipeSearch);
+      console.log(recipeSearch);
+    }
+    fetchMyAPI();
+  }, []); */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar>
+        <ToolBar>
+          <img src="https://unsplash.it/40/40" alt="" />
+        </ToolBar>
+      </AppBar>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        {console.log(data)}
+        {data &&
+          data.hits.map((item, index) => {
+            return <RecipeCard key={index} recipe={item.recipe} />;
+          })}
+      </Container>
     </div>
   );
 }
