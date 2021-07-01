7530fe60caa5151bba1ab916a94178a4de594086
Wekiban <wes.banyard@googlemail.com>
2021-07-01 09:29:49 +0200

    event listener orientation change

--- src/helperFunctions/windowResize.js
+++ src/helperFunctions/windowResize.js
@@ -11,11 +11,11 @@ export function useWindowSize() {
       setWindowSize(window.innerHeight);
     }
     // Add event listener
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('orientationchange', handleResize);
     // Call handler right away so state gets updated with initial window size
     handleResize();
     // Remove event listener on cleanup
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener('orientationchange', handleResize);
   }, []); // Empty array ensures that effect is only run on mount
   return windowSize;
 }
