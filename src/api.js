eb71149ad19815e6c916df983b2b0d99032ad4dd
Wekiban <wes.banyard@googlemail.com>
2021-07-01 19:56:40 +0200

    added catch statment

--- src/api.js
+++ src/api.js
@@ -3,10 +3,13 @@ const APP_ID = process.env.REACT_APP_EDAMAM_APP_ID;
 const url = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=`;
 
 const fetchMyApi = async (searchQuery) => {
-  let response = await fetch(url + searchQuery);
-  const data = await response.json();
-
-  return data.hits;
+  try {
+    const response = await fetch(url + searchQuery);
+    const data = await response.json();
+    return data.hits;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default fetchMyApi;
