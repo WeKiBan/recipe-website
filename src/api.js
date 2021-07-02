d8e60192212ab3776410fc04e775b5ec35b36151
Wekiban <wes.banyard@googlemail.com>
2021-07-02 13:39:34 +0200

    changed api file

--- src/api.js
+++ src/api.js
@@ -1,18 +1,13 @@
-require('dotenv').config();
 const API_KEY = process.env.REACT_APP_EDAMAM_API_KEY;
 const APP_ID = process.env.REACT_APP_EDAMAM_APP_ID;
 const url = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=`;
 
 const fetchMyApi = async (searchQuery) => {
-  try {
-    const response = await fetch(url + searchQuery);
-    const data = await response.json();
-    console.log(data.hits);
-    return data.hits;
-  } catch (err) {
-    console.log(err);
-  }
-  return [];
+  const response = await fetch(url + searchQuery);
+
+  const data = await response.json();
+
+  return data.hits;
 };
 
 export default fetchMyApi;
